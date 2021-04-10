38f9819c43273a113c4751aa8c5ed56275a8c079
BillVLad <vladshevchenko29@gmail.com>
2021-04-10 23:41:20 +0300

    КРУПНОЕ ОБНОВЛЕНИЕ: - ПОЯВИЛСЯ ДАБАБУ!!! - ТЕПЕРЬ ОН ПОВСЮДУ! ОН СОЖРЁТ ВАШИХ ДЕТЕЙ! БЕРЕГИТЕСЬ ОТ НЕГО!

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,3 +1,8 @@
+let lessvid= document.getElementById("videoless");
+
 function GithubPages() {
     window.open('https://github.com/BillVlad/billvlad.github.io');
+}
+function lessgoo() {
+    window.open('html/video.html')
 }
