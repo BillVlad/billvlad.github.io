ce3c98ae1465ddf2e2f2a3897636528cf7c5e6c1
BillVLad <vladshevchenko29@gmail.com>
2021-04-10 12:08:52 +0300

    Поменял дизайн, месторасположение и наименований скриптов для кнопки

--- javascript/bruh.js
+++
@@ -1,7 +0,0 @@
-var bruh = 'Была нажата кнопка и включается рикролл';
-var audioElement = new Audio('media/bruh.mp3');
-
-function AudioBruh() {
-    alert('Страничка откроется после того, как вы нажмёте на кнопку "OK"')
-    window.open('https://www.youtube.com/watch?v=Fve_lHIPa-I');
-}
