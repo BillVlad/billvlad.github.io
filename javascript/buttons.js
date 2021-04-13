2b9ac8ca4292335b39dce04dafa75d0c7df8e8a5
billvlad <vladshevchenko29@gmail.com>
2021-04-13 22:54:27 +0300

    JS обновлён. Добавил телеметрию

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,6 +1,9 @@
+var AudioGlek = new Audio('media/bruh.mp3')
+
 function GithubPages() {
     window.open('https://github.com/BillVlad/billvlad.github.io');
 }
-function lessgoo() {
+function bruh() {
     document.getElementById("videoless").innerHTML = '<video autoplay ><source src=./media/video0-22.mp4></video>';
+    AudioGlek.play();
 }
