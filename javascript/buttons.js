29c83bebb7498cce284ecfdb2e703e6b911ca685
BillVlad <39907880+BillVlad@users.noreply.github.com>
2021-04-14 12:17:42 +0300

    Обновил button.js
    Кнопка не работала, так как оно должно было заменить body. Я дебил

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -4,6 +4,6 @@ function GithubPages() {
     window.open('https://github.com/BillVlad/billvlad.github.io');
 }
 function bruh() {
-    document.getElementById("body").innerHTML = '<video autoplay ><source src=./media/video0-22.mp4></video>';
+    document.getElementById("lessgoo").innerHTML = '<video autoplay ><source src=./media/video0-22.mp4></video>';
     AudioGlek.play();
 }
