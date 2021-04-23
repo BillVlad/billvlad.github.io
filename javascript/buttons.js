fb56e572f0ddbc1e1e2fee7d7adc21a158cd162d
BillVLad <vladshevchenko29@gmail.com>
2021-04-23 17:44:05 +0300

    Давно не делал обновления - Сделал изменение цвета шапки с помощью input с атрибутом type=color. - И... Всё...

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,13 +1,18 @@
 var AudioGlek = new Audio('media/bruh.mp3')
+var ThesInput1 = document.getElementById("colorb")
 
 function GithubPages() {
     window.open('https://github.com/BillVlad/billvlad.github.io');
-}
+};
 function bruh() {
     document.getElementById("videoless").innerHTML = '<video class=videorick autoplay ><source src=./media/video0-22.mp4></video>';
     AudioGlek.play();
-}
+};
 function rickroll() {
     document.getElementById("rick").innerHTML = 'rick astley';
     AudioGlek.play();
-}
+};
+
+ThesInput1.addEventListener("input", function() {
+  document.getElementById("box-header").style.background = ThesInput1.value+'aa'
+    }, false);
