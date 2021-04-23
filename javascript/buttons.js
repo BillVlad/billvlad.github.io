388ba0d62815999c15a50d16d851d9854bb0e6f0
BillVLad <vladshevchenko29@gmail.com>
2021-04-23 19:05:51 +0300

    rickroll теперь бесконечный

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -8,12 +8,12 @@ function GithubPages() {
 };
 
 function easteregg() {
-    alert('Если хочешь ещё больше ора, то нажми ctrl+shift+i и открой терминал, после этого нажми на кнопку левой кнопкой мыши')
+    alert('Если хочешь ещё больше ора, то нажми ctrl+shift+i и открой терминал, после этого нажми на кнопку ПКМом')
 }
 
 function bruh() {
+    AudioGlek.loop = true;
     AudioGlek.play();
-    let i = 0
     console.log("%cВы были %cЗарикроллены", "font-size:20px", "color:red; font-size: 50px;");
     console.log("%chttps://lh3.googleusercontent.com/proxy/mzhQrN3X9WPiMTCf5UdAcTPcD2Nmv01j-HnpES58R5tLpkU2VxgpUMm6NMzA4DMjudETETO8Up9ziQ8S0MGNpqFK89A", "font-size: 10px")
 };
@@ -21,6 +21,10 @@ function bruh() {
 ThesInput1.addEventListener("input", function() {
   document.getElementById("box-header").style.background = ThesInput1.value+'aa'
     }, false);
+
+function reset() {
+    document.getElementById("box-header").style.background = '#e4e4e4b2';
+}
 
 function submit() {
     document.getElementById("content").style.color = ThesInput2.value
