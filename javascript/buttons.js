54096cba3a9adec57fa47efdad659588bdb8a8ca
BillVLad <vladshevchenko29@gmail.com>
2021-04-29 19:26:05 +0300

    Revert "test"
    This reverts commit 6f684b4bc7ae32700e4f17d8fe81273aea707a77.

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -14,9 +14,8 @@ function easteregg() {
 function bruh() {
     AudioGlek.loop = true;
     AudioGlek.play();
-    AudioGlek.volume = 0.25;
     console.log("%cВы были %cЗарикроллены", "font-size:20px", "color:red; font-size: 50px;");
-    console.log("https://github.com/BillVlad/billvlad.github.io/blob/e346d97a84bc64adfaed1216556d69d5a58e3cc0/media/unnamed.jpg", "font-size: 10px")
+    console.log("%chttps://lh3.googleusercontent.com/proxy/mzhQrN3X9WPiMTCf5UdAcTPcD2Nmv01j-HnpES58R5tLpkU2VxgpUMm6NMzA4DMjudETETO8Up9ziQ8S0MGNpqFK89A", "font-size: 10px")
 };
 
 ThesInput1.addEventListener("input", function() {
@@ -25,7 +24,7 @@ ThesInput1.addEventListener("input", function() {
 
 function reset() {
     document.getElementById("box-header").style.background = '#e4e4e4b2';
-};
+}
 
 function submit() {
     document.getElementById("content").style.color = ThesInput2.value
