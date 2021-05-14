0fe57f212436d4df8fe1f1afe7c20037ffe6f6df
BillVLad <vladshevchenko29@gmail.com>
2021-05-14 17:25:00 +0300

    Поменял логику дизайна. Теперь больше контента видеть на экране. Избавился от условий на JS и переписал условия экранов на CSS. Поддерживается живое "адаптирование"

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -3,21 +3,6 @@ var con = document.getElementById('con');
 var mobilescreen = window.screen.width <= 600;
 var desktopscreen = window.screen.width >= 600;
 
-//*initializise*//
-  /*mobile view*/
-if (mobilescreen){
-  let head = document.getElementById('box-header');
-  head.style.fontSize = '19px';
-  head.style.display = 'absolute';
-  head.style.top = '9%';
-  head.style.left = '20%';
-  head.style.right = '20%';
-  head.style.maxWidth = '700px'
-  head.style.lineHeight = '50px';
-  document.body.style.marginTop = '200px';
-}
-//*end initialization*//
-
 function keycheck() {
     if(desktopscreen){
         key.innerHTML = "<object width=1200 height=420 data='html/tester.html'></object>";
