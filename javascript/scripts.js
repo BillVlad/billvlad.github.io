f61a54f95aaee74daee6f3530182dd6dd00d5409
BillVLad <vladshevchenko29@gmail.com>
2021-05-26 22:18:17 +0300

    Error

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,11 +2,6 @@ var key = document.createElement('key');
 var con = document.getElementById('con');
 var mobilescreen = window.screen.width <= 600;
 var desktopscreen = window.screen.width >= 600;
-var dlp = document.createElement('input');
-var delp = dlp.setAttribute('type', 'checkbox');
-
-delp.style.color = 'black';
-document.body.appendChild(delp);
 
 function keycheck() {
     if(desktopscreen){
