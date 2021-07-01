460fb212c694cd4e29fe871db65ff52205069d3c
BillVlad <vladshevchenko29@gmail.com>
2021-07-01 14:54:58 +0300

    Коммит за такое долгое время

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,10 +4,10 @@ var mobilescreen = window.screen.width <= 600;
 var desktopscreen = window.screen.width >= 600;
 
 function keycheck() {
-    if(desktopscreen){
+    if (desktopscreen) {
         key.innerHTML = "<object width=1200 height=420 data='html/tester.html'></object>";
         document.querySelector('.pr').appendChild(key);
-    } else{
+    } else {
         alert("На телефоне открыть тестер клавиатуры нельзя!")
     }
 };
@@ -22,4 +22,12 @@ function openKaif(params) {
     kaif.src = 'https://cdn.discordapp.com/attachments/505783416734744579/851153172651442236/Cat_dance_Lady_Hear_Me_Tonight-k8x3ppanVwo.webm';
     kaif.autoplay = 'true';
     bio.replaceWith(kaif);
+}
+
+function isChromium() {
+    if (navigator.userAgentData.brands[1] = {"brand": "Microsoft Edge"}) {
+        console.log(`it's Edge`);
+    } else {
+    console.log(`it's not Edge`);
+    }
 }
