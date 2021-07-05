13fa46a4a6d9ec00becdcf182ccfeb3908d21c9c
BillVlad <vladshevchenko29@gmail.com>
2021-07-05 22:36:31 +0300

    Очень сильно облегчил код в JS И добавил тёмную/светлую тему

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,33 +1,21 @@
-var key = document.createElement('key');
-var con = document.getElementById('con');
-var mobilescreen = window.screen.width <= 600;
-var desktopscreen = window.screen.width >= 600;
-
-function keycheck() {
+document.querySelector('.check').addEventListener('click', () => {
+    let key = document.createElement('key'); key.id = "keyb";
+    let desktopscreen = window.screen.width >= 600;
     if (desktopscreen) {
         key.innerHTML = "<object width=1200 height=420 data='html/tester.html'></object>";
         document.querySelector('.pr').appendChild(key);
     } else {
         alert("На телефоне открыть тестер клавиатуры нельзя!")
     }
-};
+});
 
-function keystop() {
-    key.remove(key);
-};
+document.querySelector('.rst').onclick = () => document.querySelector('#keyb').remove();
 
-function openKaif(params) {
+document.querySelector('#kaif').addEventListener('click', () => {
     let bio = document.querySelector('#bio');
     let kaif = document.createElement('video');
     kaif.src = 'https://cdn.discordapp.com/attachments/505783416734744579/851153172651442236/Cat_dance_Lady_Hear_Me_Tonight-k8x3ppanVwo.webm';
     kaif.autoplay = 'true';
+    kaif.volume = 0.10;
     bio.replaceWith(kaif);
-}
-
-function isChromium() {
-    if (navigator.userAgentData.brands[1] = { "brand": "Microsoft Edge" }) {
-        console.log(`it's Edge`);
-    } else {
-        console.log(`it's not Edge`);
-    }
-}
+});
